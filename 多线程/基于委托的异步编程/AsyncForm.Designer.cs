1cbc07a615099282951a634c894ed8c3cb161752
hc2014 <294836881@qq.com>
2016-12-16 16:11:29 +0800

    异步

---
+++ 多线程/基于委托的异步编程/AsyncForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace 基于委托的异步编程
+{
+    partial class AsyncForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "异步执行";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 45);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "利用回调同步执行多个任务";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "夸线程操作控件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // AsyncForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 281);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AsyncForm";
+            this.Text = "异步执行";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 多线程/基于委托的异步编程/AsyncForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 基于委托的异步编程
+{
+    public partial class AsyncForm : Form
+    {
+        public AsyncForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "正在计算...";
+            //声明委托
+            Func<int, int> result = new Func<int, int>(Async);
+
+            IAsyncResult ires = result.BeginInvoke(10, null, null);
+
+            label2.Text = Sync(10).ToString();
+
+            label1.Text = result.EndInvoke(ires).ToString();
+        }
+
+        private int Sync(int num)
+        {
+            return num * num;
+        }
+        private int Async(int num)
+        {
+            Thread.Sleep(5000);
+            return num * num;
+        }
+
+        private int CallbackTest(int num1, int num2)
+        {
+            Thread.Sleep(num2);
+            return num1 * num1;
+        }
+
+        
+        //声明委托
+        Func<int, int, int> result=null;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //实例委托
+            result = new Func<int, int, int>(CallbackTest);
+
+            //创建10个任务
+            for (int i = 1; i < 11; i++)
+            {
+                result.BeginInvoke(10 * i, 1000 * i, MyCallBack, i);
+            }
+        }
+
+        private void MyCallBack(IAsyncResult irs)
+        {
+            int res = result.EndInvoke(irs);
+            Console.WriteLine("当前执行第{0}个任务,执行结果是:{1}",irs.AsyncState.ToString(),res);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadTest));
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        void ThreadTest()
+        {
+            int a = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                a += i;
+                if (label1.InvokeRequired)
+                {
+                    label1.Invoke(new Action<string>(s=>label1.Text=s),a.ToString());
+
+                }
+                //如果直接这样写会报错
+                //label1.Text=a.ToString();
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
